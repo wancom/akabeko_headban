b343f34e31d5906d9c50ce32b5e921de665c6882
Y.N <s1240095@gmail.com>
2018-08-24 20:00:52 +0900

    Fix motor palse sleep timer(sec -> msec)

--- connect_Raspi/comp.py
+++ connect_Raspi/comp.py
@@ -15,64 +15,63 @@ tmptime=30.0
 try:
     while True:
 
-#	print GPIO.input(21)
-        if GPIO.input(20) == True:
-	    game.init()
-	    game.music.load('../music/akabeko1.mp3')
-	    game.music.play(1)
-	    GPIO.output(23,True)
-	    intime=time.time()
-	    nowtime=intime
+    if GPIO.input(20) == True:
+        game.init()
+        game.music.load('../music/akabeko1.mp3')
+        game.music.play(1)
+        GPIO.output(23,True)
+        intime=time.time()
+        nowtime=intime
 
-	    while nowtime - intime < tmptime:
-	        GPIO.output(12,True)
-		print GPIO.input(12)
-		time.sleep(60.0/160)
-		GPIO.output(12,False)
-		time.sleep(60.0/160)
-		nowtime=time.time()
-	    
-	    game.music.stop()
-        
-	if GPIO.input(21) == True:
-	    game.init()
-	    game.music.load('../music/akabeko2.mp3')
-	    game.music.play(1)
-	    GPIO.output(18,True)
-	    intime=time.time()
-	    nowtime=intime
+        while nowtime - intime < tmptime:
+            GPIO.output(12,True)
+            print GPIO.input(12)
+            time.sleep(60.0/160/1000)
+            GPIO.output(12,False)
+            time.sleep(60.0/160/1000)
+            nowtime=time.time()
 
-	    while nowtime - intime < tmptime:
-	        GPIO.output(12,True)
-		print GPIO.input(12)
-		time.sleep(116.0/160)
-		GPIO.output(12,False)
-		time.sleep(116.0/160)
-		nowtime=time.time()
-	    
-	    game.music.stop()
-	
-	if GPIO.input(24) == True:
-	    game.init()
-	    game.music.load('../music/akabeko3145.mp3')
-	    game.music.play(1)
-	    GPIO.output(17,True)
-	    intime=time.time()
-	    nowtime=intime
+        game.music.stop()
 
-	    while nowtime - intime < tmptime:
-		print GPIO.input(12)
-		time.sleep(145.0/160)
-		GPIO.output(12,False)
-		time.sleep(145.0/160)
-		nowtime=time.time()
-	    
-	    game.music.stop()
-	else:
-	    GPIO.output(23,False)
-	    GPIO.output(17,False)
-	    GPIO.output(18,False)
-	    time.sleep(0.1)
+    if GPIO.input(21) == True:
+        game.init()
+        game.music.load('../music/akabeko2.mp3')
+        game.music.play(1)
+        GPIO.output(18,True)
+        intime=time.time()
+        nowtime=intime
+
+        while nowtime - intime < tmptime:
+            GPIO.output(12,True)
+            print GPIO.input(12)
+            time.sleep(116.0/160/1000)
+            GPIO.output(12,False)
+            time.sleep(116.0/160/1000)
+            nowtime=time.time()
+
+        game.music.stop()
+
+    if GPIO.input(24) == True:
+        game.init()
+        game.music.load('../music/akabeko3145.mp3')
+        game.music.play(1)
+        GPIO.output(17,True)
+        intime=time.time()
+        nowtime=intime
+
+        while nowtime - intime < tmptime:
+            print GPIO.input(12)
+            time.sleep(145.0/160/1000)
+            GPIO.output(12,False)
+            time.sleep(145.0/160/1000)
+            nowtime=time.time()
+
+        game.music.stop()
+    else:
+        GPIO.output(23,False)
+        GPIO.output(17,False)
+        GPIO.output(18,False)
+        time.sleep(0.1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
